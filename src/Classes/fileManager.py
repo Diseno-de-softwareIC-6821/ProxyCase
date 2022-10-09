e0560729459535405197bc9ac159410cfb775ed2
Daniel Araya Sambucci <danielarayasambucci@estudiantec.cr>
2022-10-09 16:55:33 -0600

    FileManager
    Saves and loads both logs.txt and record.txt

---
+++ src/Classes/fileManager.py
@@ -0,0 +1,32 @@
+import pathlib
+
+class FileManager():
+    def __init__(self):
+        # Get the current path, solving problem saving files on same fileManager.py path
+        self.path = str((pathlib.Path(__file__).parent.resolve()))
+        self.path = self.path[:self.path.find("src")] + "files\\"
+    
+    def loadTxt(self, mode):
+        txtData = ""
+        try:
+            if (mode == 1):
+                txt = open(self.path + "logs.txt", "r")
+            elif (mode == 2):
+                txt = open(self.path + "record.txt", "r")
+            txtData = txt.read()
+            txt.close()
+        except:
+            print("Error: File not found")
+        return txtData
+
+    def saveTxt(self, data, mode):
+        newTxt = data + "\n"
+        try:
+            if (mode == 1):
+                txt = open(self.path + "logs.txt", "a")
+            elif (mode == 2):
+                txt = open(self.path + "record.txt", "a")
+            txt.write(newTxt)
+            txt.close()
+        except:
+            print("Error saving file")
--- src/main.py
+++ src/main.py
@@ -1,6 +1,13 @@
-def main() -> None:
-    print("hello world")
+import pathlib
+import sys
 
+sys.path.append(str((pathlib.Path(__file__).parent.resolve())) + "\\Classes") 
+
+import fileManager as fm
+
+def main() -> None:
+    fileManager = fm.FileManager()
+    print(fileManager.loadTxt(1))
 
 if __name__ == "__main__":
     main()
